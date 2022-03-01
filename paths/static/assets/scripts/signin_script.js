d631b20c1cca8c9e8d619968034da08e21c5ca46
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-03-01 16:01:40 +0800

    displaying password

--- paths/static/assets/scripts/signin_script.js
+++ paths/static/assets/scripts/signin_script.js
@@ -1,4 +1,10 @@
 
+// Load image './assets/svg/eye-solid.svg'
+const openedEyeIcon = document.createElement('img');
+openedEyeIcon.src = './assets/svg/eye-solid.svg';
+const closedEyeIcon = document.createElement('img');
+closedEyeIcon.src = './assets/svg/eye-slash-solid.svg';
+
 const loginCard = document.getElementById('loginCard');
 const centerLoginCard = () => {
     const height = loginCard.offsetHeight;
@@ -12,6 +18,31 @@ const centerLoginCard = () => {
 };
 centerLoginCard();
 
+const passwordContainer = document.getElementById('passwordContainer');
+const eyeIcon = document.getElementById('eyeIcon');
+const passwordInput = document.querySelector('input[name="password"]');
+const resizeEyeIcon = () => {
+    const height = passwordContainer.offsetHeight;
+    passwordInput.style.width = `${passwordContainer.offsetWidth - height}px`;
+    eyeIcon.style.height = `${height/2}px`;
+    eyeIcon.style.marginTop = `${height/4}px`;
+    eyeIcon.style.marginLeft = `${height/4}px`;
+};
+resizeEyeIcon();
+
+eyeIcon.onclick = () => {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.src = openedEyeIcon.src;
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.src = closedEyeIcon.src;
+    };
+    passwordInput.focus();
+    resizeEyeIcon();
+};
+
 window.onresize = () => {
     centerLoginCard();
+    resizeEyeIcon();
 };
