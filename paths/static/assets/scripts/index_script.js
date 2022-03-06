abd5d123cabe6f829c3c5cb6ee4641b13ad1e811
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-03-06 21:07:24 +0800

    [partial] dashboard layout

--- paths/static/assets/scripts/index_script.js
+++ paths/static/assets/scripts/index_script.js
@@ -2,9 +2,62 @@
 // Get the port
 const port = location.port;
 
-// Connect to WebSocket server
-const socket = io();
+// // Connect to WebSocket server
+// const socket = io();
 
-socket.on('connect', () => {
-    console.log(`Connected to the server as: ${socket.id}`);
-});
+// socket.on('connect', () => {
+//     console.log(`Connected to the server as: ${socket.id}`);
+// });
+
+
+
+const sidePanel = document.getElementById('sidePanel');
+const mainPanel = document.getElementById('mainPanel');
+const menuButton = document.getElementById('menuButton');
+
+// Resize the Main Panel
+const resizeMainPanel = async () => {
+    mainPanel.style.height = `${sidePanel.offsetHeight}px`;
+
+    // Get the sidePanel display attribute
+    const displayAttribute = sidePanel.getAttribute('display');
+    if (displayAttribute === 'false') {
+        mainPanel.style.height = `${sidePanel.offsetHeight}px`;
+        mainPanel.style.width = `${window.innerWidth}px`;
+        mainPanel.style.position = 'absolute';
+        return;
+    };
+
+    // Media query
+    if (window.innerWidth <= 795) {
+        mainPanel.style.width = '100%';
+        mainPanel.style.position = 'absolute';
+        mainPanel.style.left = '0px';
+        return;
+    };
+    mainPanel.style.width = `${window.innerWidth - sidePanel.offsetWidth}px`;
+    mainPanel.style.position = 'relative';
+};
+
+// Displaying the side panel
+menuButton.onclick =  async () => {
+    // Get the sidePanel display attribute
+    const displayAttribute = sidePanel.getAttribute('display');
+    if (displayAttribute === 'true') {
+        sidePanel.setAttribute('display', 'false');
+        sidePanel.style.marginLeft = `-${sidePanel.offsetWidth}px`;
+        menuButton.style.right = `-${menuButton.offsetWidth}px`;
+    } else {
+        sidePanel.setAttribute('display', 'true');
+        sidePanel.style.marginLeft = '0px';
+        menuButton.style.right = '0px';
+    };
+    resizeMainPanel();
+};
+
+
+
+resizeMainPanel();
+window.onresize =  async () => {
+    resizeMainPanel();
+};
