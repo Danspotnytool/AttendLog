12a8359dc1fbf6d3ed5d036fd4dd30d6989ca774
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-03-12 19:59:54 +0800

    class creation or something idk

--- api/routes/classes.js
+++ api/routes/classes.js
@@ -23,11 +23,273 @@ const getClasses = async () => {
         classesArray.forEach((classs) => {
             classes.push(classs);
         });
+        console.log('Classes database is loaded');
+    });
+    // Listen to changes in the database
+    // Child added
+    database.ref('/classes').on('child_added', (snapshot) => {
+        // Check if this class is already in the array
+        // The structure of the array is:
+        // [
+        //     {
+        //         id: '',
+        //         className: '',
+        //         createdAt: '',
+        //     }
+        // ]
+        const classs = snapshot.val();
+        const classExists = classes.find(classFromArray => classFromArray.classID === classs.classID);;
+        if (!classExists) {
+            classes.push(classs);
+        };
+    });
+    // Child removed
+    database.ref('/classes').on('child_removed', (snapshot) => {
+        const classs = snapshot.val();
+        const classIndex = classes.findIndex(classFromArray => classFromArray.classID === classs.classID);
+        if (classIndex > -1) {
+            classes.splice(classIndex, 1);
+        };
     });
 };
 getClasses();
 
 
+
 // Create a new class
+router.post('/create', async (req, res) => {
+    const { className, classColor, classDescription } = req.body;
+    // Validate the token on the header cookie
+    const user = {
+        userID: '',
+        token: ''
+    };
+    try {
+        user.userID = JSON.parse(req.headers.cookie).userID;
+        user.token = JSON.parse(req.headers.cookie).token;
+    } catch (error) {
+        console.log(error);
+        return res.send({
+            message: 'Invalid Authorization',
+            code: '400'
+        });
+    };
+    // Validate the token on the database
+    const userRef = database.ref(`/users/${user.userID}`);
+    await userRef.once('value', (snapshot) => {
+        if (snapshot.val().token !== user.token) {
+            return res.send({
+                message: 'Invalid Authorization',
+                code: '400'
+            });
+        };
+    });
+
+    // Create the class
+    const classs = {
+        classID: `${Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))}`,
+        className: className,
+        classColor: classColor,
+        classDescription: classDescription,
+        createdAt: Date.now(),
+        classTeacher: user.userID,
+        classToken: uuidv4()
+    };
+
+    // Add the class to the database
+    database.ref(`/classes/${classs.classID}`).set(classs);
+
+    // Add the class to the user's classes array
+    // Get the contents of the user's classes array
+    const userClasses = [];
+    await database.ref(`/users/${user.userID}/classes`).once('value', (snapshot) => {
+        // Check if the user has any classes
+        if (snapshot.val()) {
+            const userClassesArray = Object.values(snapshot.val());
+            userClassesArray.forEach((userClass) => {
+                userClasses.push(userClass);
+            });
+        };
+    });
+    // Add the class to the user's classes array
+    userClasses.push(classs.classID);
+    // Update the user's classes array
+    await database.ref(`/users/${user.userID}/classes`).set(userClasses);
+
+    // Send the response
+    return res.send({
+        message: 'Class created',
+        code: '200',
+        class: classs
+    });
+});
+
+// Getting user's classes
+router.get('/get', async (req, res) => {
+    // Validate the token on the header cookie
+    const user = {
+        userID: '',
+        token: ''
+    };
+    try {
+        user.userID = JSON.parse(req.headers.cookie).userID;
+        user.token = JSON.parse(req.headers.cookie).token;
+    } catch (error) {
+        console.log(error);
+        return res.send({
+            message: 'Invalid Authorization',
+            code: '400'
+        });
+    };
+    // Validate the token on the database
+    const userRef = database.ref(`/users/${user.userID}`);
+    await userRef.once('value', (snapshot) => {
+        if (snapshot.val().token !== user.token) {
+            return res.send({
+                message: 'Invalid Authorization',
+                code: '400'
+            });
+        };
+        // Assign the user's classes to the response
+        user['classes'] = snapshot.val().classes;
+    });
+
+    // Get the user's classes
+    const userClasses = [];
+    // Check if the user has any classes
+    if (user.classes) {
+        user.classes.forEach((userClass) => {
+            // Get the class from the database
+            database.ref(`/classes/${userClass}`).once('value', (snapshot) => {
+                userClasses.push(snapshot.val());
+            });
+        });
+        // Get the name of the teacher and number of students for each class
+        const userClassesWithTeacher = [];
+        for (const classs of userClasses) {
+            let teacherName = '';
+            let numberOfStudents = 0;
+            await database.ref(`/users/${classs.classTeacher}`).once('value', (snapshot) => {
+                teacherName = snapshot.val().username;
+            });
+            await database.ref(`/classes/${classs.classID}/students`).once('value', (snapshot) => {
+                // Check if the class has any students
+                if (snapshot.val()) {
+                    const studentsArray = Object.values(snapshot.val());
+                    numberOfStudents = studentsArray.length;
+                };
+            });
+            classs['teacherName'] = teacherName;
+            classs['numberOfStudents'] = numberOfStudents;
+            userClassesWithTeacher.push(classs);
+        };
+        // Map the userClassesWithTeacher and remove the classToken
+        const userClassesWithTeacherMapped = userClassesWithTeacher.map(classs => {
+            delete classs.classToken;
+            return classs;
+        });
+        // Send the response
+        return res.send(userClassesWithTeacherMapped); 
+    };
+    res.send({
+        message: 'No classes found',
+        code: '404'
+    });
+});
+
+
+
+// Join a class
+router.post('/join', async (req, res) => {
+    // Validate the token on the header cookie
+    const user = {
+        userID: '',
+        token: '',
+        classes: []
+    };
+    try {
+        user.userID = JSON.parse(req.headers.cookie).userID;
+        user.token = JSON.parse(req.headers.cookie).token;
+    } catch (error) {
+        console.log(error);
+        return res.send({
+            message: 'Invalid Authorization',
+            code: '400'
+        });
+    };
+    // Validate the token on the database
+    const userRef = database.ref(`/users/${user.userID}`);
+    await userRef.once('value', (snapshot) => {
+        if (snapshot.val().token !== user.token) {
+            return res.send({
+                message: 'Invalid Authorization',
+                code: '400'
+            });
+        };
+        // Assign the user's classes to the response
+        if (snapshot.val().classes) {
+            user.classes = snapshot.val().classes;
+        };
+    });
+    console.log(user);
+
+    // Get the request body
+    const { classID, classToken } = req.body;
+
+    // Validate the classID and classToken
+    database.ref(`/classes/${classID}`).once('value', async (snapshot) => {
+        // Check if the class exist
+        if (!snapshot.val()) {
+            return res.send({
+                message: 'Class not found',
+                code: '404'
+            });
+        };
+        // Check if the classToken is valid
+        if (snapshot.val().classToken !== classToken) {
+            return res.send({
+                message: 'Invalid classToken',
+                code: '400'
+            });
+        };
+        // Check if the user is already in the class
+        if (user.classes.length > 0) {
+
+        } else if (user.classes.includes(classID)) {
+            return res.send({
+                message: 'User already in the class',
+                code: '400'
+            });
+        };
+        // Add the user to the class
+        // The class's students array
+        const students = [];
+        // Get the class's students array
+        await database.ref(`/classes/${classID}/students`).once('value', (snapshot) => {
+            // Check if the class has any students
+            if (snapshot.val()) {
+                const studentsArray = Object.values(snapshot.val());
+                studentsArray.forEach((student) => {
+                    students.push(student);
+                });
+            };
+        });
+        // Add the user to the class's students array
+        students.push(user.userID);
+        // Update the class's students array
+        await database.ref(`/classes/${classID}/students`).set(students);
+
+        // Add the class to the user's classes array
+        user['classes'].push(classID);
+        // Update the user's classes array
+        await database.ref(`/users/${user.userID}/classes`).set(user['classes']);
+
+        // Send the response
+        return res.send({
+            message: 'User added to class',
+            code: '200'
+        });
+    });
+});
 
 module.exports = router;
--- paths/static/assets/scripts/dashboard_script.js
+++ paths/static/assets/scripts/dashboard_script.js
@@ -2,13 +2,21 @@
 // Get the port
 const port = location.port;
 
-// // Connect to WebSocket server
+// // Connect to Socket.IO server
 // const socket = io();
 
 // socket.on('connect', () => {
 //     console.log(`Connected to the server as: ${socket.id}`);
 // });
-
+// socket.on('disconnect', () => {
+//     console.log('Disconnected from the server');
+//     // Distroy the socket
+//     socket.destroy();
+// });
+// socket.on('error', (error) => {
+//     console.log(error);
+//     socket.destroy();
+// });
 
 
 
@@ -19,6 +27,12 @@ const menuButton = document.getElementById('menuButton');
 const menuButtonBarIcons = [];
 const header = document.getElementById('header');
 const formsBackground = document.getElementById('formsBackground');
+const createClass = document.getElementById('createClass');
+const createClassForm = document.getElementById('createClassForm');
+const createClassButton = document.getElementById('createClassButton');
+const joinClass = document.getElementById('joinClass');
+const joinClassForm = document.getElementById('joinClassForm');
+const joinClassButton = document.getElementById('joinClassButton');
 
 // Load the menuButton svg
 fetch('./assets/svg/menu-icon.svg')
@@ -140,22 +154,41 @@ window.onresize =  async () => {
 
 
 // Displaying the Create Class form
-const createClass = document.getElementById('createClass');
-const createClassForm = document.getElementById('createClassForm');
 const displayCreateClassForm = async () => {
-    createClassForm.style.top = '10%';
+    createClassForm.style.top = '5%';
     createClassForm.style.transform = 'scale(100%)';
     formsBackground.style.display = 'block';
 };
 createClass.onclick = () => { displayCreateClassForm(); };
 
+// Display the Join Class form
+const displayJoinClassForm = async () => {
+    joinClassForm.style.top = '5%';
+    joinClassForm.style.transform = 'scale(100%)';
+    formsBackground.style.display = 'block';
+};
+joinClass.onclick = () => { displayJoinClassForm(); };
+
 // Hide the forms
 const hideForms = async () => {
     createClassForm.style.top = '-100%';
     createClassForm.style.transform = 'scale(0%)';
+    joinClassForm.style.top = '-100%';
+    joinClassForm.style.transform = 'scale(0%)';
     formsBackground.style.display = 'none';
+
+    // Clear all form fields
+    Array.from(document.getElementsByTagName('form')).forEach((form) => {
+        form.reset();
+    });
 };
 formsBackground.onclick = () => { hideForms(); };
+Array.from(document.getElementsByClassName('closeFormIcon')).forEach((element) => {
+    element.onclick = () => { hideForms(); };
+});
+Array.from(document.getElementsByClassName('cancel')).forEach((element) => {
+    element.onclick = () => { hideForms(); };
+});
 
 
 
@@ -202,3 +235,169 @@ updateTime();
 setInterval(() => {
     updateTime();
 }, 1000);
+
+
+// Get user's classes
+const getUserClasses = async () => {
+    // Errase the classes
+    const classContainer = document.getElementById('classContainer');
+    if (classContainer.children.length > 0) {
+        while (classContainer.firstChild) {
+            classContainer.removeChild(classContainer.firstChild);
+        };
+    };
+
+    fetch('/api/classes/get', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+    .then(data => {
+        if (data.code === '400') {
+            return console.log(data.message);
+        } else {
+            const classes = Array.from(data);
+            const classContainer = document.getElementById('classContainer');
+            classes.forEach((classData) => {
+                const classCard = document.createElement('div');
+                classCard.className = 'classCard';
+                classCard.style.backgroundColor = `${classData.classColor}`;
+
+                // Check if the class color is dark or light
+                const color = classData.classColor;
+                let r = parseInt(color.substring(1, 3), 16);
+                let g = parseInt(color.substring(3, 5), 16);
+                let b = parseInt(color.substring(5, 7), 16);
+                const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+                const titleCard = document.createElement('div');
+                if (brightness < 125) {
+                    titleCard.style.color = 'white';
+                    const newR = Math.round(r * 0.8).toString(16);
+                    const newG = Math.round(g * 0.8).toString(16);
+                    const newB = Math.round(b * 0.8).toString(16);
+                    const newColor = `#${newR}${newG}${newB}`;
+                    classCard.style.backgroundColor = `${newColor}`;
+                } else {
+                    titleCard.style.color = 'black';
+                    const newR = Math.round(r * 1.2).toString(16);
+                    const newG = Math.round(g * 1.2).toString(16);
+                    const newB = Math.round(b * 1.2).toString(16);
+                    const newColor = `#${newR}${newG}${newB}`;
+                    classCard.style.backgroundColor = `${newColor}`;
+                };
+                titleCard.className = 'titleCard';
+
+                classCard.appendChild(titleCard);
+
+                const title = document.createElement('h3');
+                title.innerHTML = `${classData.className}`;
+                titleCard.appendChild(title);
+
+                const infoCard = document.createElement('div');
+                infoCard.className = 'infoCard';
+                titleCard.appendChild(infoCard);
+
+                const teacherName = document.createElement('h4');
+                teacherName.innerText = `${classData.teacherName}`;
+                infoCard.appendChild(teacherName);
+
+                const studentsNumber = document.createElement('h4');
+                studentsNumber.innerText = `${classData.numberOfStudents || 0}`;
+                infoCard.appendChild(studentsNumber);
+
+                const description = document.createElement('p');
+                description.className = 'description';
+                description.innerText = `${classData.classDescription}`;
+                if (brightness < 125) {
+                    description.style.color = 'white';
+                } else {
+                    description.style.color = 'black';
+                };
+                classCard.appendChild(description);
+
+
+                classContainer.appendChild(classCard);
+            });
+        };
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+getUserClasses();
+
+
+
+// Creating the class
+createClassButton.onclick = async (event) => {
+    event.preventDefault();
+    createClassButton.disabled = true;
+
+    // Get the form data
+    const formData = new FormData(createClassForm);
+    const className = formData.get('className');
+    const classColor = formData.get('classColor');
+    const classDescription = formData.get('classDescription');
+    
+    // Create the class
+    fetch('/api/classes/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            className: className,
+            classColor: classColor,
+            classDescription: classDescription
+        })
+    }).then(response => response.json())
+    .then(data => {
+        if (data.code === '400') {
+            return console.log(data.message);
+        } else {
+            getUserClasses();
+            formsBackground.click();
+            createClassForm.reset();
+        };
+    }).catch(err => {
+        console.log(err);
+    });
+};
+
+
+// Join Class
+joinClassButton.onclick = async (event) => {
+    event.preventDefault();
+    joinClassButton.disabled = true;
+
+    // Get the form data
+    const formData = new FormData(joinClassForm);
+    const classID = formData.get('classID');
+    const classToken = formData.get('classToken');
+
+    // Join the class
+    fetch('/api/classes/join', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            classID: classID,
+            classToken: classToken
+        })
+    }).then(response => response.json())
+    .then(data => {
+        if (data.code === '400') {
+            // If the class is not found
+        } else {
+            getUserClasses();
+            formsBackground.click();
+            joinClassForm.reset();
+            // Click the forms background to close the forms
+            formsBackground.click();
+        };
+    }).catch(err => {
+        console.log(err);
+    });
+};
