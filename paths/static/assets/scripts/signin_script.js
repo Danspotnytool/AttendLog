e7642612f424e21b9f9c1bf655fefb975d04ea12
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-03-23 11:29:15 +0800

    redesign signin form

--- paths/static/assets/scripts/signin_script.js
+++ paths/static/assets/scripts/signin_script.js
@@ -4,11 +4,16 @@
 document.cookie = '{}';
 
 // Get rem function
+/**
+ * @param {number} rem
+ */
 const getRem = (rem) => {
     if (!rem) { return parseFloat(getComputedStyle(document.documentElement).fontSize) };
     if (isNaN(rem)) { throw new Error('rem is not defined'); };
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 };
+
+
 
 // Load image './assets/svg/eye-solid.svg'
 const openedEyeIcon = document.createElement('img');
@@ -16,33 +21,31 @@ openedEyeIcon.src = './assets/svg/eye-solid.svg';
 const closedEyeIcon = document.createElement('img');
 closedEyeIcon.src = './assets/svg/eye-slash-solid.svg';
 
-// Center the loginCard
-const loginCard = document.getElementById('loginCard');
-const centerLoginCard = () => {
-    const height = loginCard.offsetHeight;
-    const width = loginCard.offsetWidth;
 
-    const winH = window.innerHeight;
-    const winW = window.innerWidth;
 
-    loginCard.style.marginTop = `${(winH - height) / 2}px`;
-    loginCard.style.marginLeft = `${(winW - width) / 2}px`;
-};
-
-// Reposition the Eye Icon
+const welcomeCard = document.getElementById('welcomeCard');
+const signinCard = document.getElementById('signinCard');
 const passwordContainer = document.getElementById('passwordContainer');
+const passwordInput = document.getElementById('password');
 const eyeIcon = document.getElementById('eyeIcon');
-const passwordInput = document.querySelector('input[name="password"]');
-const moveEyeIcon = () => {
-    const containerHeight = passwordContainer.offsetHeight;
-    eyeIcon.style.top = `${(containerHeight/2) - (eyeIcon.offsetHeight/2)}px`;
-    eyeIcon.style.right = `${eyeIcon.offsetWidth/2}px`;
 
-    // Update the pasword input padding
-    passwordInput.style.paddingRight = `${eyeIcon.offsetWidth + eyeIcon.offsetWidth/2}px`;
+
+
+// Resize welcome card
+const resizeWelcomeCard = () => {
+    welcomeCard.style.width = `${signinCard.offsetLeft}px`;
 };
 
-// Hiding and Showing the password to the screen
+// Move eye icon
+const moveEyeIcon = () => {
+    eyeIcon.style.display = 'block';
+    eyeIcon.style.top = `${(passwordContainer.offsetHeight / 2) - (eyeIcon.offsetHeight / 2)}px`;
+    eyeIcon.style.right = `${getRem(1)}px`;
+};
+
+
+
+// Display password
 eyeIcon.onclick = () => {
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
@@ -51,34 +54,32 @@ eyeIcon.onclick = () => {
         passwordInput.type = 'password';
         eyeIcon.src = closedEyeIcon.src;
     };
-    passwordInput.focus();
-    moveEyeIcon();
 };
 
 
-// Call all move and resize functions
-centerLoginCard();
-moveEyeIcon();
-setTimeout(() => {
-    centerLoginCard();
+
+window.onload = () => {
+    resizeWelcomeCard();
     moveEyeIcon();
-}, 0);
-// Listen for window resize
+};
 window.onresize = () => {
-    centerLoginCard();
+    resizeWelcomeCard();
     moveEyeIcon();
 };
+
+
 
 // Remove window.prompt
 delete window.prompt;
 
-const signin = document.getElementById('signin');
+
 const signinForm = document.getElementById('signinForm');
+const signin = document.getElementById('signin');
 const prompt = document.getElementById('prompt');
 
 signin.onclick = (event) => {
     event.preventDefault();
-
+    
     signin.disabled = true;
 
     const username = document.getElementById('username').value;
