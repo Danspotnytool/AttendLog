b633461cb5b1927eae7d83782beebd7930b10ca3
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-03-23 12:49:15 +0800

    fix sign out button

--- paths/static/assets/scripts/dashboard_script.js
+++ paths/static/assets/scripts/dashboard_script.js
@@ -112,7 +112,7 @@ menuButton.onclick = () => { displaySidePanel(); };
 signOutButton.onclick = () => {
     localStorage.removeItem('user');
     document.cookie = '{}';
-    window.location.href = `http://localhost:${port}/signin`;
+    window.location.href = `${window.location.origin}${port ? `:${port}` : ''}/signin`;
 };
 
 
